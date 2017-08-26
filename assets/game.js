e3bc22380b7355c63f296486b194bf8050317abb
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-08-26 00:50:45 -0400

    created directory, files, and coded random generator for game

---
+++ assets/game.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+var random1 = Math.floor((Math.random) * 13);
+var random2 = Math.floor((Math.random)* 121);
+
+
+$('#random-button').on('click', function() {
+    console.log('clicked', works);
+    var random = Math.floor((Math.random) * 13);
+    $('#random-number').html(none).css(display, none);
+    console.log('run', test);
+    });
+});
