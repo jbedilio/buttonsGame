3e48ee260e24740a2ed8b48f5bea143d9f42abb0
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-08-26 17:41:22 -0400

    worked with  JQ rendering the buttons and value

--- assets/game.js
+++ assets/game.js
@@ -1,13 +1,41 @@
 $(document).ready(function () {
 
-var random1 = Math.floor((Math.random) * 13);
-var random2 = Math.floor((Math.random)* 121);
+var $deal = Math.floor((Math.random)() * 13);
+
+var $wins = 0;
+var $losses = 0;
+
+var $need = Math.floor((Math.random)() * 121);
+
+var $bs = ['#b1','#b2','#b3','#b4']
 
 
-$('#random-button').on('click', function() {
-    console.log('clicked', works);
-    var random = Math.floor((Math.random) * 13);
-    $('#random-number').html(none).css(display, none);
+
+
+$('#need').html('You Need: ', + $need);
+console.log($need);
+
+//$.each(button)
+
+/*$('#b1, #b2, #b3, #b4').on('click', function() {
+    console.log('clicked');
+    var buttons = Math.floor((Math.random)() * 13);
+    $('#b1', '#b2', '#b3', '#b4') = buttons;
     console.log('run', test);
-    });
+    });*/
+
+var $b = $('#buttons');
+$.each($bs, function (index, button) {
+    console.log('button', index, button);
+    var $button = $('<button>');
+    $button.html(button);
+    $b.append($button);
+});
+
+/*var $drinkLists = $('#drink-options');
+$.each($drinklist, function (index, drink) {
+    console.log('a drink', index, drink);
+    var $li = $('<li>');
+    $li.html(drink);
+    $drinkList.append($li);*/
 });
