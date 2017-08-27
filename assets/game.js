bc2a07ffff499893c0e7eb7379c421c2601bd91e
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-08-26 21:09:12 -0400

    completed random generated numbers and loop genenrated buttons and value assignments

--- assets/game.js
+++ assets/game.js
@@ -1,36 +1,42 @@
 $(document).ready(function () {
-
 var $deal = Math.floor((Math.random)() * 13);
-
 var $wins = 0;
 var $losses = 0;
-
-var $need = Math.floor((Math.random)() * 121);
-
+var $need = (Math.floor((Math.random)() * 101)+19);
 var $bs = ['#b1','#b2','#b3','#b4']
+var $score = 0;
 
-
-
-
-$('#need').html('You Need: ', + $need);
+$('#need').html('You Need: ' + $need);
 console.log($need);
-
-//$.each(button)
-
-/*$('#b1, #b2, #b3, #b4').on('click', function() {
-    console.log('clicked');
-    var buttons = Math.floor((Math.random)() * 13);
-    $('#b1', '#b2', '#b3', '#b4') = buttons;
-    console.log('run', test);
-    });*/
 
 var $b = $('#buttons');
 $.each($bs, function (index, button) {
+    var $deal = Math.floor((Math.random)() * 13);
     console.log('button', index, button);
-    var $button = $('<button>');
+    var $button = $('<button>')
+    .addClass('btn')
+    .attr('value', $deal);
     $button.html(button);
     $b.append($button);
 });
+
+
+
+$('.btn').on('click', function() {
+    var num2 = $(this).val();
+    $score = parseFloat($score) + parseFloat(num2);
+    $('#score').html('Your Score: ' + $score);
+    if ($score == $need){
+        $wins++;
+        $('#wins').html('Wins:' + $wins);
+        $need = (Math.floor((Math.random)() * 101) + 19);
+        $('#need').html('You Need:' + $need);
+
+    }
+
+})
+
+
 
 /*var $drinkLists = $('#drink-options');
 $.each($drinklist, function (index, drink) {
