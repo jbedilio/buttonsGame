c716b8ce4feec88ac684015a7186ff325c85ad8c
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-08-26 23:28:44 -0400

    finished programing the jq for the game

--- assets/game.js
+++ assets/game.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-var $deal = Math.floor((Math.random)() * 13);
+var $deal = (Math.floor((Math.random)() * 12)+1);
 var $wins = 0;
 var $losses = 0;
 var $need = (Math.floor((Math.random)() * 101)+19);
-var $bs = ['#b1','#b2','#b3','#b4']
+var $bs = ['b1','b2','b3','b4']
 var $score = 0;
 
 $('#need').html('You Need: ' + $need);
@@ -11,16 +11,14 @@ console.log($need);
 
 var $b = $('#buttons');
 $.each($bs, function (index, button) {
-    var $deal = Math.floor((Math.random)() * 13);
+    var $deal = (Math.floor((Math.random)() * 12)+1);
     console.log('button', index, button);
     var $button = $('<button>')
-    .addClass('btn')
+    .addClass('btn ' + $bs[index])
     .attr('value', $deal);
     $button.html(button);
     $b.append($button);
 });
-
-
 
 $('.btn').on('click', function() {
     var num2 = $(this).val();
@@ -31,10 +29,25 @@ $('.btn').on('click', function() {
         $('#wins').html('Wins:' + $wins);
         $need = (Math.floor((Math.random)() * 101) + 19);
         $('#need').html('You Need:' + $need);
-
+        $score = 0;
+        $('#score').html('Your Score:' + $score);
+        $.each($bs, function (index, button) {
+        $deal = (Math.floor((Math.random)() * 12)+1);
+        $('.' + $bs[index]).attr('value', $deal);
+        });
+    }else if ($score > $need){
+        $losses++;
+        $('#losses').html('losses:' + $losses);
+        $need = (Math.floor((Math.random)() * 101) + 19);
+        $('#need').html('You Need:' + $need);
+        $score = 0;
+        $('#score').html('Your Score:' + $score);
+        $.each($bs, function (index, button) {
+        $deal = (Math.floor((Math.random)() * 12) + 1);
+        $('.' + $bs[index]).attr('value', $deal);
+    });
     }
-
-})
+});
 
 
 
@@ -45,3 +58,8 @@ $.each($drinklist, function (index, drink) {
     $li.html(drink);
     $drinkList.append($li);*/
 });
+
+/*$.each($bs, function (index, button) {
+    var $deal = Math.floor((Math.random)() * 13)
+        .attr('value', $deal);
+});*/
